664701d0cd511033410a460d4e34ad5a5b7cedb6
Seyit Murat Kaya <murat.kaya1269@gmail.com>
2023-10-16 15:08:08 +0300

    Initial Commit

---
+++ FoodOrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrderApp
+//
+//  Created by Seyit Murat Kaya on 16.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
