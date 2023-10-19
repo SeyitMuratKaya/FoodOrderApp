4a3646fa7009a83afcb415f1af7eb410ddd90aec
Seyit Murat Kaya <murat.kaya1269@gmail.com>
2023-10-19 18:01:21 +0300

    add tap animation to list items

--- FoodOrderApp/Components/LargeItemCollectionViewCell.swift
+++ FoodOrderApp/Components/LargeItemCollectionViewCell.swift
@@ -162,3 +162,33 @@ class LargeItemCollectionViewCell: UICollectionViewCell {
     }
 
 }
+
+extension LargeItemCollectionViewCell {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        DispatchQueue.main.async {
+            self.alpha = 1.0
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveLinear, animations: {
+                self.alpha = 0.5
+            }, completion: nil)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        DispatchQueue.main.async {
+            self.alpha = 0.5
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveLinear, animations: {
+                self.alpha = 1.0
+            }, completion: nil)
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        DispatchQueue.main.async {
+            self.alpha = 0.5
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveLinear, animations: {
+                self.alpha = 1.0
+            }, completion: nil)
+        }
+    }
+}
+
--- FoodOrderApp/Components/LargeListItemView.swift
+++ FoodOrderApp/Components/LargeListItemView.swift
@@ -153,3 +153,32 @@ class LargeListItemView: UIView {
         ])
     }
 }
+
+extension LargeListItemView {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        DispatchQueue.main.async {
+            self.alpha = 1.0
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveLinear, animations: {
+                self.alpha = 0.5
+            }, completion: nil)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        DispatchQueue.main.async {
+            self.alpha = 0.5
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveLinear, animations: {
+                self.alpha = 1.0
+            }, completion: nil)
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        DispatchQueue.main.async {
+            self.alpha = 0.5
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveLinear, animations: {
+                self.alpha = 1.0
+            }, completion: nil)
+        }
+    }
+}
